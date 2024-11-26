a3c723b0bcbe04a7f79eec6827ef46de1e8659bb
Sagnik Hore <sagnikhoredht@gmail.com>
2024-11-26 11:13:28 +0530

    jj

--- src/Accenture/NumFreq.java
+++ src/Accenture/NumFreq.java
@@ -15,12 +15,13 @@ import java.util.*;
     public static void PrintFreq(ArrayList<Integer> arr,int n){
         Map<Integer,Integer> mpp = new HashMap<>();
         for (int i = 0; i < n; i++) {
-            if(mpp.containsKey(arr.get(i))){
-                mpp.put(arr.get(i),mpp.get(arr.get(i))+1 );
-            }
-            else{
-                mpp.put(arr.get(i),1);
-            }
+//            if(mpp.containsKey(arr.get(i))){
+//                mpp.put(arr.get(i),mpp.get(arr.get(i))+1 );
+//            }
+//            else{
+//                mpp.put(arr.get(i),1);
+//            }
+            mpp.put(arr.get(i),mpp.getOrDefault(arr.get(i),0)+1);
         }
         for(Map.Entry<Integer,Integer> entry : mpp.entrySet()){
             System.out.println(entry.getKey() +" occurs "+entry.getValue() +" times");
